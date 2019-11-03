fc6161807976db578b8bff81f3081a3f572e357a
trawzified <tsdezwart@protonmail.com>
2019-11-03 21:42:46 +0100

    Rename Assembly name and Default namespace to SkyrimShredder

--- SkyrimShredder/Properties/AssemblyInfo.cs
+++ SkyrimShredder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WabbajackModlistPreperator")]
+[assembly: AssemblyTitle("SkyrimShredder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WabbajackModlistPreperator")]
+[assembly: AssemblyProduct("SkyrimShredder")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
